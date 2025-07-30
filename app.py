c068cee9e081d11589eed57f21d9f7ee9bab9d31
ImhariaUghu <imhariaosoria@gmail.com>
2025-07-30 18:18:21 +0100

    Fix CORS config

--- app.py
+++ app.py
@@ -138,6 +138,13 @@ def express_correlation():
     correlation = np.corrcoef(weights, costs)[0, 1]
     return jsonify({"correlation": round(correlation, 4)})
 
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', 'http://localhost:3000')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    return response
+
 # Run the Flask app
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
